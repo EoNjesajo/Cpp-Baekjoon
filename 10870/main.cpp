4d4ae08a6bf7017da4b85c6be08e69944bf8e99e
EoNjesajo <dnjsrua59@gmail.com>
2022-08-22 12:43:33 +0900

    first commit

---
+++ 10870/main.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+
+int f[48] = {0,1,};
+int fib(int n){
+
+}
+
+int main(){
+    std::ios_base::sync_with_stdio(false);
+
+}
+
+// int main(){
+//     std::ios_base::sync_with_stdio(false);
+//     int f[] = {0, 1};
+//     int n;
+//     std::cin >> n;
+//     if(n<2){
+//         std::cout << f[n] << "\n";
+//         return 0;
+//     }
+//     for(int i=2; i<=n; i++){
+//         int temp = f[0] + f[1];
+//         f[0] = f[1];
+//         f[1] = temp;
+//     }
+//     std::cout << f[1] << "\n";
+// }
