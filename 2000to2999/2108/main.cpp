bcf76f2d255ca87da6918506b6198c11b8ebcaa6
EoNjesajo <dnjsrua59@gmail.com>
2022-09-19 11:56:12 +0900

    first commit

--- 2000to2999/2108/main.cpp
+++ 2000to2999/2108/main.cpp
@@ -1,63 +1,104 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-#include<iomanip>
+#include <iostream>
+#include <algorithm>
+#include <vector>
 #include <cmath>
 
-void print_aver(std::vector<int> number){
-    int sum = 0;
-    for(auto ele : number)
-        sum+=ele;
-    double aver = std::round((double)sum/number.size());
-    if(aver==-0)
-        aver = -aver;
-    std::cout << aver <<"\n";
-}
-void print_median(std::vector<int> number){
-    int median_index = (number.size()-1)/2;
-    std::cout << number[median_index] << "\n";
-}
-void print_mode(std::vector<int> number){
-    int mode, count, mode_count = 0, flag = 1;
-    for(auto ele : number){
-        count = std::count(number.begin(), number.end(), ele);
-        if(mode_count < count){
-            mode_count = count;
-            mode = ele;
-            flag = 1;
-        }else if(flag && mode!=ele&&mode_count == count){
-            mode = ele;
-            flag = 0;
-        }
-    }
-    std::cout << mode << "\n";
-}
+int main() {
+    int t, tmp, range, median = 0, mode, aver = 0, flag = 0;
+    std::vector<int> input;
+    int mode_count = -9999;
+    int number[8001] = {0,};
 
-void print_range(int min, int max){
-    if(min==max)
-        std::cout << 0 << "\n";
-    else{
-        int range = (min<0) ? max-min : max+min;
-        std::cout << range << "\n"; 
-    }
-}
-
-int main(){
-    std::ios_base::sync_with_stdio(false);
-
-    int t, tmp, min, max;
-    std::vector<int> number;
     std::cin >> t;
     while(t--){
-        std::cin>>tmp;
-        number.push_back(tmp);
+        std::cin >> tmp;
+        input.push_back(tmp);
+        aver += tmp;
+        number[4000+tmp]++;
     }
-    std::sort(number.begin(), number.end());
-    min = *number.begin();
-    max = *(number.end()-1);
-
-    print_aver(number);
-    print_median(number);
-    print_mode(number);
-    print_range(min, max);
+    sort(input.begin(),input.end());
+    for(int i = 0; i < 8001; i++){
+        if(number[i] == 0)
+            continue;
+        if(number[i] == mode_count){
+            if(flag){
+                mode = i - 4000;
+                flag = false;
+            }
+        }
+        if(number[i] > mode_count){
+            mode_count = number[i];
+            mode = i - 4000;
+            flag = 1;
+        }
+    }
+    median = input[input.size()/2];
+    aver = round((float)aver / input.size());
+    range = input.back() - input.front();
+    std::cout << aver << '\n' << median << '\n' << mode << '\n' << range;
 }
+
+
+// #include<iostream>
+// #include<vector>
+// #include<algorithm>
+// #include<iomanip>
+// #include <cmath>
+
+// void print_aver(std::vector<int> number){
+//     int sum = 0;
+//     for(auto ele : number)
+//         sum+=ele;
+//     double aver = std::round((double)sum/number.size());
+//     if(aver==-0)
+//         aver = -aver;
+//     std::cout << aver <<"\n";
+// }
+// void print_median(std::vector<int> number){
+//     int median_index = (number.size()-1)/2;
+//     std::cout << number[median_index] << "\n";
+// }
+// void print_mode(std::vector<int> number){
+//     int mode, count, mode_count = 0, flag = 1;
+//     for(auto ele : number){
+//         count = std::count(number.begin(), number.end(), ele);
+//         if(mode_count < count){
+//             mode_count = count;
+//             mode = ele;
+//             flag = 1;
+//         }else if(flag && mode!=ele&&mode_count == count){
+//             mode = ele;
+//             flag = 0;
+//         }
+//     }
+//     std::cout << mode << "\n";
+// }
+
+// void print_range(int min, int max){
+//     if(min==max)
+//         std::cout << 0 << "\n";
+//     else{
+//         int range = (min<0) ? max-min : max+min;
+//         std::cout << range << "\n"; 
+//     }
+// }
+
+// int main(){
+//     std::ios_base::sync_with_stdio(false);
+
+//     int t, tmp, min, max;
+//     std::vector<int> number;
+//     std::cin >> t;
+//     while(t--){
+//         std::cin>>tmp;
+//         number.push_back(tmp);
+//     }
+//     std::sort(number.begin(), number.end());
+//     min = *number.begin();
+//     max = *(number.end()-1);
+
+//     print_aver(number);
+//     print_median(number);
+//     print_mode(number);
+//     print_range(min, max);
+// }
