b6318f5ac215b4560880c8c5fac35a0cc43a9261
EoNjesajo <dnjsrua59@gmail.com>
2022-09-26 23:44:16 +0900

    first commit

---
+++ 3000to3999/3046/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <algorithm>
+ 
+int main()
+{
+	int n, avg;
+	int result;
+ 
+	std::cin >> n >> avg;
+ 
+	result = (avg * 2) - n;
+ 
+	std::cout << result << "\n";
+
+}
